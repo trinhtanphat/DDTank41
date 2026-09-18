5766c24fe2c755a0754576c791b3841b802953f3
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-09-18 07:13:29 +0700

    feat(treasure): restore packet commands

---
+++ Game.Server/Packets/TreasurePackageType.cs
@@ -0,0 +1,13 @@
+namespace Game.Server.Packets
+{
+    public enum TreasurePackageType
+    {
+        IN_TREASURE = 0,
+        ARRANGE_FRIEND_FARM = 1,
+        END_TREASURE = 2,
+        DIG = 3,
+        LOGIN_ABOUT_TREASURE = 4,
+        BEREPAIR_FRIEND_FARM_SEND = 5,
+        START_GAME = 6
+    }
+}
